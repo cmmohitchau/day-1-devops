98e7bc9ceeb14353d31c69f5517436887252059b
cmmohitchau <cmmohitchau@gmail.com>
2026-02-03 17:32:16 +0530

    added packages'

---
+++ packages/db/index.ts
@@ -0,0 +1,13 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client";
+
+console.log("url  : " , process.env.DATABASE_URL);
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+
+export const prisma = new PrismaClient({
+  adapter,
+});
